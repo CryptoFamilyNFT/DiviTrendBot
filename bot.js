a3c85613211befcb0822f82e7ea9ad828f624e68
CryptoFamilyNFT <giuseppediloreto7@protonmail.com>
2024-02-10 18:38:07 +0100

    MINT section – #done

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
-const serverless = require('serverless');
+const serverless = require('serverless-http');
 
 const fs = require('fs');
 const { prefix } = require('./config.json');
