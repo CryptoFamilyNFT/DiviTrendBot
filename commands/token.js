a56713c81e7efd88f45136f7d7bdf3930edb5aa3
CryptoFamilyNFT <giuseppediloreto7@protonmail.com>
2024-03-27 20:07:51 +0100

    fix

--- commands/token.js
+++ commands/token.js
@@ -1,11 +1,13 @@
-const { ethers } = require("ethers");
-const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const { openseaAssetUrl } = require('../config.json');
 const { contractAddress, ABI } = require('../config.json');
 const axios = require("axios");
 const { Client, Intents } = require('discord.js');
+const Discord = require('discord.js');
+const { ethers } = require("ethers");
 
 module.exports = {
-	name: 'fact',
+	name: process.env.DISCORD_TOKEN_COMMAND || "token",
 	async execute(message, args) {
 		if (!args.length) {
 			return message.channel.send(`You didn't provide a token id, ${message.author}!`);
@@ -25,10 +27,10 @@ module.exports = {
 
         async function getImageUrl(tokenId) {
             try {
-                const uri = await contract.baseURI(tokenId);
+                const uri = await contract.tokenURI(tokenId);
                 console.log("Base URI:", uri);
                 
-                const response = await axios.get(uri);
+                const response = await axios.get('https://ipfs.filebase.io/ipfs/' + uri.slice(7));
                 console.log("Response data:", response.data);
         
                 const json = response.data;
@@ -44,8 +46,22 @@ module.exports = {
 
 		try {
             console.log("tokenId", tokenId)
+			const owner = await contract.ownerOf(tokenId);
+			const ownerAddress = owner.toString();
+
+			const imageUrl = await getImageUrl(tokenId);
+
+			const embedMsg = new Discord.MessageEmbed()
+				.setColor('#0099ff')
+				.setTitle(`Divitrend Factories ${tokenId}`)
+				.setURL(imageUrl)
+				.addField("Owner: ", ownerAddress)
+				.setImage(imageUrl);
+
+			message.channel.send(embedMsg);
 		} catch (error) {
             console.log(error)
+			message.channel.send(tokenId);
 		}
 	},
 };
--- cronjobs/mint.js
+++ cronjobs/mint.js
@@ -1,6 +1,6 @@
 const { ethers } = require("ethers");
 const Discord = require('discord.js');
-const { contractAddress, ABI721, RewardsABI } = require('../config.json');
+const { contractAddress, ABI } = require('../config.json');
 const axios = require("axios");
 
 module.exports = {
@@ -11,40 +11,38 @@ module.exports = {
     once: false, // Ascolta l'evento più volte
 
     async execute(client) {
-        const provider = new ethers.providers.JsonRpcProvider("https://polygon-rpc.com");
-        const contract = new ethers.Contract('0x034d89CD11a548fc4E4D7C365B0460fa6895De67', ABI721, provider);
+        const provider = new ethers.providers.JsonRpcProvider("https://rpc.ankr.com/arbitrum");
+        const contract = new ethers.Contract('0x35029f03602454a6149b353dd8d227c4f2d99b7c', ABI, provider);
         console.log("Provider and contract initialized");
 
         // Funzione per gestire l'evento NftMinted
-        async function handleTransferEvent(event) {
-            const from = event.args[0];
-            const to = event.args[1];
-            const tokenId = event.args[2].toString(); // Convert BigNumber to string 
+        async function handleMintedEvent(event) {
+            const account = event.args[0];
+            const tokenIds = event.args[1];
+            console.log("TOKEND IDS = ", tokenIds)
             const transactionHash = event.transactionHash;
+            console.log(event)
 
-            console.log("New Transfer event - Token ID:", tokenId);
-            console.log("From:", from);
-            console.log("To:", to);
-            console.log("Transaction Hash:", transactionHash);
+            tokenIds.forEach(async (tokenId) => {
+                try {
+                    const image = 'https://ipfs.filebase.io/ipfs/' + 'QmPejW5kfFj67JccPCpXcazyehhxNgrg2h1WR6burL9UDj/' + tokenId + '.png'
+                    console.log("New event - Image URL:", image);
 
-            try {
-                const image = 'https://ipfs.filebase.io/ipfs/' + 'QmPejW5kfFj67JccPCpXcazyehhxNgrg2h1WR6burL9UDj/' + tokenId + '.png'
-                console.log("New event - Image URL:", image);
+                    const embedMsg = new Discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle("Divitrend Factories #" + tokenId)
+                        .setURL(image)
+                        .setDescription(`[${account.slice(0, 5)}...] has just minted: $FACT #${tokenId}`)
+                        .setThumbnail(image)
+                        .addField("FROM", `[${account}]`, true)
+                        .addField("TX HASH:", `[${transactionHash}]`, true);
 
-                const embedMsg = new Discord.MessageEmbed()
-                    .setColor('#0099ff')
-                    .setTitle("Divitrend Factories #" + tokenId)
-                    .setURL(image)
-                    .setDescription(`[${account.slice(0, 5)}...] has minted #${tokenId}`)
-                    .setThumbnail(image)
-                    .addField("FROM", `[${account}]`, true)
-                    .addField("TX HASH:", `[${transactionHash}]`, true);
-
-                const channel = await client.channels.fetch(process.env.DISCORD_LISTING_CHANNEL_ID);
-                channel.send(embedMsg);
-            } catch (error) {
-                console.error(error);
-            }
+                    const channel = await client.channels.fetch(process.env.DISCORD_LISTING_CHANNEL_ID);
+                    channel.send(embedMsg);
+                } catch (error) {
+                    console.error(error);
+                }
+            });
         }
 
         // Funzione per controllare gli eventi dei blocchi
@@ -59,7 +57,7 @@ module.exports = {
                 console.log("Checking for events from block", fromBlock, "to block", toBlock); // Log per i blocchi che stiamo controllando
 
                 // Ottieni tutti gli eventi nei blocchi specificati
-                contract.on("Transfer", (account, tokenIds, event) => {
+                contract.on("NftMinted", (account, tokenIds, event) => {
                     console.log("New event - Token IDs:", tokenIds);
                     handleMintedEvent({ args: [account, tokenIds], transactionHash: event.transactionHash });
                 });
